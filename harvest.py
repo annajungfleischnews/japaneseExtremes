c85811514c3ab9be0051c620bcbbc9b3bafcec66
KMicha <michael.kahle@yahoo.de>
2026-01-13 16:40:47 +0100

    add diagrams

--- harvest.py
+++ harvest.py
@@ -13,6 +13,7 @@ import aiohttp
 import asyncio
 import requests
 from urllib.parse import urlparse
+import urllib
 import json
 import time
 import smtplib
@@ -28,8 +29,6 @@ from dateutil import parser
 ##from datetime import date, timedelta, datetime, timezone
 
 from CreLanguageTranslate.LanguageTranslate import LanguageTranslate 
-
-# pip3 install deep-translator
 from deep_translator import GoogleTranslator
 
 DATA_PATH = Path.cwd()
@@ -127,7 +126,7 @@ if(not keywordsNewsDF.empty):
   print(keywordsNewsDF2)
   keywordsNewsDF2['counting'] = keywordsNewsDF2['title'].fillna(0)
   keywordsNewsDF2['counting'] = keywordsNewsDF2['counting'] - keywordsNewsDF2['ratio']
-  keywordsNewsDF2 = keywordsNewsDF2.sort_values(by=['counting'], ascending=True)  
+  keywordsNewsDF2 = keywordsNewsDF2.sort_values(by=['counter','counting'], ascending=True)  
 
 rows20 = int(math.ceil(keywordsNewsDF2.shape[0]/5))
 keywordsNewsDF2 = keywordsNewsDF2.head(rows20)
@@ -455,8 +454,6 @@ def countSingleCharsInQuote(keyword, quote, case=True):
       return countFound/countAll
     return 0
 
-
-
 def checkKeywordInQuote(keyword, quote, case=True, anyKey=False, singleChars=False):
     keyword = keyword.replace("+","").replace("-","")
     keywords = keyword.strip("'").split(" ")
@@ -473,7 +470,6 @@ def checkKeywordInQuote(keyword, quote, case=True, anyKey=False, singleChars=Fal
       allFound = True
       for keyw in keywords:
         allFound = allFound and (keyw in quote)  
-
     return allFound
 
 def checkArticlesForKeywords(articles, termsDF, seldomDF, language, keyWord, topic, feed, country, ipcc, continent):
@@ -526,7 +522,22 @@ def checkArticlesForKeywords(articles, termsDF, seldomDF, language, keyWord, top
              valid = max(valid,0.6) 
       if(not found):
         for index2, column2 in termsLangDF.iterrows(): 
-           allFound = checkKeywordInQuote(keyword, fullQuote, case=True, anyKey=True)
+           allFound = checkKeywordInQuote(keyword, fullQuote, case=False)
+           if(allFound):
+             foundKeywords.append(keyword) 
+             found = True
+             valid = max(valid,0.55) 
+      if(not found):
+        for index2, column2 in termsLangDF.iterrows(): 
+           allFound = checkKeywordInQuote(keyword, searchQuote+fullQuote, case=True, anyKey=True)
+           if(allFound):
+             foundKeywords.append(keyword) 
+             foundColumns.append(column2) 
+             found = True
+             valid = max(valid,0.3) 
+      if(not found):
+        for index2, column2 in termsLangDF.iterrows(): 
+           allFound = checkKeywordInQuote(keyword, searchQuote+fullQuote, case=False, anyKey=True)
            if(allFound):
              foundKeywords.append(keyword) 
              foundColumns.append(column2) 
@@ -614,6 +625,53 @@ def getLatestFileAge():
     return minAge        
 
 
+ts = int(time.time())
+currentMonths = []
+for m in [0,20,40,60]:
+  ##month = datetime.utcfromtimestamp(ts-60*60*24*m).strftime('%Y_%m')  
+  month = datetime.datetime.fromtimestamp(ts-60*60*24*m).strftime('%Y_%m')
+  if month not in currentMonths:
+    currentMonths.append(month)
+
+def inqMailNews():
+    foundNew = False
+    keyWord = 'veryUnusualAndNeverUsedKeyword'
+    language = os.getenv('EXTREME_LANGUAGE')
+    if (language == 'xx'):
+      print('Please set EXTREME_LANGUAGE in file: mysecrets.py');
+      return None
+    for currMonth in currentMonths:
+       extremesFile = "https://github.com/pg-ufr-news/mailHarvester/blob/main/csv/extreme/"+language+"/news_"+currMonth+".csv?raw=true"
+       print(extremesFile)
+       extremesRequest = requests.get(extremesFile, headers={'Accept': 'text/plain'})
+       print(extremesRequest)   
+       if(extremesRequest.status_code == 200):
+          extremesDf=pd.read_csv(io.StringIO(extremesRequest.content.decode('utf-8')), delimiter=',', index_col='index')
+          ##extremesDf['hash'] = extremesDf.index 
+          print(extremesDf)
+          extremesDf = extremesDf[extremesDf['language']==language]
+          extremesDf['feed'] = 'mail'
+          print(extremesDf)
+          extremesDict = extremesDf.to_dict('index')
+          extremesArray = list(extremesDict.values())
+          print(extremesArray)
+          checkedArticles = checkArticlesForKeywords(extremesArray, keywordsDF, keywordsNewsDF2, language, keyWord)          
+          print(checkedArticles)
+          newArticles = filterNewAndArchive(checkedArticles, language, keyWord)    
+          for data in newArticles:
+                    if (dataIsNotBlocked(data)):                    
+                        #print(str(keyWord)+': '+str(title)+' '+str(url))
+                        print(["addNewsToCollection: ",data])
+                        if(addNewsToCollection(data)):
+                            foundNew = True
+                            print(["+++added"])  
+                        else:
+                            print(["---not added"])   
+
+       
+    if(foundNew):         
+       storeCollection()
+
 def inqRandomNews(maxCount=1):
   #global termsDF
   global termsDF3
@@ -631,7 +689,9 @@ def inqRandomNews(maxCount=1):
     if(termsDF3.ratio.max()>0.77):
       randomNumber = 0.5   
     if(unsearchedTerms.ratio.max()>0.750):    #0.765(?)  #0.759:36;3; 0.758:55;21 , 0.757:83;47 , 0.75:215
-      randomNumber = 0.1   
+      randomNumber = 0.1  
+    if(termsDF3.counter.min()<0.5):
+      randomNumber = 0.98   
     ## randomNumber = 0.1 # unsearched first
     #randomNumber = 0.5 # succesors first
     #randomNumber = 0.7
@@ -699,7 +759,7 @@ def inqRandomNews(maxCount=1):
         # https://newsapi.org/docs/endpoints/everything
         url = ('https://newsapi.org/v2/everything?'+
             #"q='"+keyWord+"'&"
-            "q="+keyWord+"&"
+            "q="+urllib.parse.quote(keyWord)+"&"
             'pageSize='+str(pageSize)+'&'
             'language='+nLang+'&'
             'page='+str(currPage)+'&'
@@ -762,9 +822,16 @@ def inqRandomNews(maxCount=1):
                 if(foundNew):     
                     maxCount  -= (2 + len(newArticles))  
                     storeCollection()
+             else:
+               #0 articles found
+               print(['0 articles found, counter:',newCounter ,'ratio: ',currRatio, 'pages:', newLimit])
             else:
               print(response.text)
               if(jsonData['code'] == 'maximumResultsReached'):
+                deltaLimit = -1
+                maxCount = 0
+                newLimit =  max(1,currPage+deltaLimit)
+              if(jsonData['code'] == 'rateLimited'):
                 deltaLimit = -1
                 maxCount = 0
                 newLimit =  max(1,currPage+deltaLimit)
@@ -802,6 +869,7 @@ def inqRandomNews(maxCount=1):
 #              "publishedAt":"2021-07-16T15:06:00Z",
 #              "content":"Nach den langen und heftigen Regenf\xc3\xa4llen Mitte der Woche treten immer mehr katastrophale Folgen zutage: Die Zahl der Toten w\xc3\xa4chst, allein in Rheinland-Pfalz und Nordrhein-Westfalen sind \xc3\xbcber 100 Mens\xe2\x80\xa6 [+4840 chars]"}
 
+inqMailNews()
 
 amount = 4
 if(len(termsDF)>50):
--- import.py
+++ import.py
@@ -30,7 +30,7 @@ termsFields = ["index","module", "topic", "color", "feed", "term", "created", "c
 topicsDict = {}
 
 # MOVE TO myparameters.py (no secrets only)
-MAX_IMPORTS = 10
+MAX_IMPORTS = 8
 
 TARGET_LANGUAGE = os.getenv('EXTREME_LANGUAGE')
 if(TARGET_LANGUAGE == 'xx'): 
@@ -84,8 +84,11 @@ def importTerms(maxImports=10, targetLanguage='de'):
     topicsDF['ratio'] *= topicsDF['country'].apply(
       lambda x: max(0.5,countriesForLanguage[x]) if (x in countriesForLanguage) else 0.4 
     )
+    topicsDF['ratio'] += topicsDF['feed'].apply(
+      lambda x: 0.11 if (x == 'random') else 0.0
+    )
     topicsDF = topicsDF.sort_values(by=['ratio'], ascending=False) 
-    topicsDF['pages'] = 2
+    topicsDF['pages'] = 1
     topicsDF['counter'] = 0
 
     topicsDF['color'] = topicsDF['topic'].apply(
